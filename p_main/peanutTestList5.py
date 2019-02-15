d5b69142a272fff739e08c04bbecaa6cfd93e78c
TheLurkiest <bng5@wildcats.unh.edu>
2019-02-15 07:43:02 -0500

    added the remaining 9 steps to our step by step basic guide

--- p_main/peanutTestList5.py
+++ p_main/peanutTestList5.py
@@ -14,7 +14,51 @@ print("the list said this for the last three values: " + str(p_list[-3:]))
 l1 = ['1)Choose which variety of peanuts to plant in your plot of land-- Choose from one of the following Cultivar Groups:', 'incomplete',
 'a) Spanish Group (commonly grown in South Africa): Spanish peanuts are most often used to make peanut butter and candy because of their higher oil content. The Spanish variety produces small nuts with a brownish-red skin.  Spanish peanuts need about 90 to 130 days for maturity.','learnmore123',
 '1b) Virginia Group : The Jumbo Virginia or Early Virginia variety has the largest nuts of all the types. This is a common roasting variety that needs between 120 and 130 days to produce a crop.','learnmore123',
-'1c) Tennessee Red Valencia Group: Valencia peanuts are sweet and typically roasted or boiled. This variety produces three or more nuts per pod, and it takes between 120 and 130 days to reach maturity.','learnmore123','taskbreak']
+'1c) Tennessee Red Valencia Group: Valencia peanuts are sweet and typically roasted or boiled. This variety produces three or more nuts per pod, and it takes between 120 and 130 days to reach maturity.','learnmore123','-------------------', 'taskbreak']
+
+l1 = ['1)Choose which variety of peanuts to plant in your plot of land-- Choose from one of the following Cultivar Groups:', 'incomplete',
+'a) Spanish Group (commonly grown in South Africa): Spanish peanuts are most often used to make peanut butter and candy because of their higher oil content. The Spanish variety produces small nuts with a brownish-red skin.  Spanish peanuts need about 90 to 130 days for maturity.','learnmore123',
+'1b) Virginia Group : The Jumbo Virginia or Early Virginia variety has the largest nuts of all the types. This is a common roasting variety that needs between 120 and 130 days to produce a crop.','learnmore123',
+'1c) Tennessee Red Valencia Group: Valencia peanuts are sweet and typically roasted or boiled. This variety produces three or more nuts per pod, and it takes between 120 and 130 days to reach maturity.' ,'learnmore123','-------------------', 'taskbreak',
+'2) Set up the number and location of all plots of land you want to start growing on and use this starting info to start a new thing in your API:' , 'incomplete',
+'2) Site Preparation- Choose a plot of land to grow on: Choose a site with light, sandy soil amended with compost and other organic material. Peanuts do not grow well in clay or poor-draining soil.' ,'learnmore123','-------------------', 'taskbreak',
+'3) Pre-planting soil preparation (optional):' ,'learnmore123',
+'3a) Compost application and clearing growth-inhibiting obstructions: If necessary, work 2 to 4 inches of compost into the soil at a depth of about 6 inches. Rake the site to remove weeds, rocks and other debris.' , 'incomplete',
+'3b) soil pH testing and management: You may choose to have your soil tested to make amendments for the peanut plants. Ideal soil pH ranges between 5.8 and 6.2. If your soil has a low pH, raise it with a bit of lime.' , 'incomplete',
+'3c) calcium deficiency management: Peanuts also need calcium, so amend the soil with agricultural gypsum if your soil has a lack of the mineral.' , 'incomplete',
+'3d) Potential Considerations for high N Fertilizer Application (should be applied at least 1-2 weeks before adding peanut seeds to soil): Often, peanuts bought from nurseries are treated with a nitrogen-fixing bacteria that allows the plants to make their own nitrogen. Such plants do not need extra fertilizer. If your plants were not treated, apply 1 pound of 20-0-0 or 16-16-16 fertilizer for every 100 square feet of peanut plants.', 'learnmore123', '-------------------', 'taskbreak',
+'4) Planting- Check to see (by continually monitoring) if the conditions are right for you to begin planting-- move on to step 5 where you will plant your peanuts once the following conditions are met:' ,'learnmore123',
+'4a) Be aware (and enter in) your area’s last frost date, and enter that into your device so your API can plan your planting schedule accordingly: Plant your peanuts after your area\'s last frost date,' , 'incomplete',
+'4b) As a secondary concern, try to make sure that the soil temperature is above 65 degrees Fahrenheit before planting: when the soil temperature is above 65 degrees Fahrenheit.' ,'learnmore123','-------------------', 'taskbreak',
+'5) Procedure for Planting:' , 'incomplete',
+'5a) first apply inoculant to seeds immediately prior to planting in the following way: Simply place the seeds in a pan, add enough water to moisten them, and add a small amount of inoculant to the water. Leave the seeds in their a little while until they’ve had time to soak, stirring the seeds with a stick periodically while they soak until they have a little powder on their seed coats and then plant them immediately. It’s not an exact science, but an 8 ounce bag of peanut inoculant should be enough to treat about 100 pounds of seeds, so if you’re just growing on a small scale (like for a small personal garden), you could probably just add a large spoonful of inoculant to have more than enough for a single small plot containing a dozen or more peanut plants within it.' ,'learnmore123',
+'5b) Then put the seeds into the soil according to these standard conventions: Plant peanuts in mounds, sowing four peanuts in each mound. Sow the peanuts 1 1/2 inches deep and between 6 to 18 inches apart. If you want multiple rows of peanuts, space the rows 24 inches apart. Water thoroughly until the soil feels moist. Germination usually occurs within five to 10 days, and within 25 to 40 days after sowing, yellow flowers form. Upon flower pollination, plants send pegs down into the soil. The peanuts grow on these pegs underground.' ,'learnmore123','-------------------', 'taskbreak',
+'6) Normal Watering Schedule: Water peanut plants regularly; 1 to 2 inches of water each week is usually sufficient, except in the hottest weather when plants may need more. If you water irregularly, plants will suffer.', '-------------------', 'taskrepeat_weekly',
+'8) Hilling: When peanut plants grow to about 1 foot tall, mound 6 inches of soil around the bottom of each plant. This process, called hilling, provides better drainage and more room for peanut pods to grow. Put 3 to 4 inches of compost between peanut plants after hilling.','incomplete','-------------------', 'taskbreak',
+'9) Responding to significant weed encroachment: If weeds are a problem in your garden, apply up to 3 inches of mulch around peanut plants as a protective barrier. The mulch prevents many weeds from growing.','incomplete','-------------------', 'taskbreak',
+'10) Harvest- Monitoring to determine when peanut plants are ready to harvest: typically around your area\'s first frost date (enter this into API or use generic default) you should begin checking to see if your peanut plant’s leaves have started to turn yellow-- when this happens it’s time to pull them up for harvesting.','incomplete','-------------------', 'taskbreak',
+'10a) allow them to dry out for one or two weeks:','incomplete','-------------------', 'taskbreak',
+'The proper day to harvest these things is now assigned as:' ,'learnmore123',
+'the yellowing_day + 7 days…. And the end of the proper harvest period is yellowing_day + 14 days-- so you have a week to get it done and the API will be notifying you of this every day until it’s marked as complete.' ,'learnmore123',
+'Leave the peanuts on the plants, and allow them to dry out for one or two weeks.' ,'learnmore123',
+'10b) pull peanuts off plants and put in trays:' ,'learnmore123',
+'After the initial drying period, pull the peanuts off the plants and place them in shallow trays. Let them dry for two or three weeks more.' , 'incomplete',
+'A humidity level below 70 percent and temperatures under 95 degrees Fahrenheit are ideal for drying.' ,'learnmore123',
+'10c) Roast peanuts in oven: If desired, roast the peanuts in the oven at 350 degrees Fahrenheit for about 13 to 18 minutes.' , 'incomplete',
+'10d) Store left-over peanuts until expiration deadline is reached: Put the peanuts in an air-tight container and refrigerate them for six to 12 months. Consume unrefrigerated peanuts within four weeks.','incomplete','-------------------', 'taskbreak' ]
+
+
+# 7) our regular weekly schedule function will pretty much run coninuously for our code after it's been reached-- or rather, it'll start up there, once the 'regular_sched_started' bool has been reached and triggered.  We will communicate back to the API to check if this is the case
+# if all the regular weekly scheduled tasks have been completed, THEN you get returned back to our normal while loop.
+
+# when 'taskrepeat_weekly' gets hit we then call the function called weekly_sched() which accepts our list of parameters and parses them (all but the last 2 which are true/false) until it finds one that ISN'T <= -9990
+# ...and when it DOES find one, they'll rush that through a conditional, which will tell our API whether that is something the user should be notified or not, and which transfers any changes made here by the user BACK to the API as well
+
+# regular_sched_started = False
+
+def weekly_sched():
+	print("7) Regular weekly schedule commencing............................................ placeholder text until function is finished")
+
 
 
 p_reply='zzz'
@@ -49,6 +93,24 @@ while(count<len(l1)):
 				not_done = 0
 				count = old_count - 1
 				print('returning to old count now which is: ' + str(count))
+	if(l1[count] =='taskrepeat_weekly'):
+		weekly_sched()
+		#print('debbbbbbbuuuuuuuuuuuug')
+		if(not_done == 0):
+			print('all sub-tasks complete-- moving on to next task...')
+		else:
+			p_reply=input('not all sub-tasks have been completed.  Would you like to move on to the next task anyway? (y/n): ')
+			if(p_reply == 'y'):
+				old_count = count + 1
+				count = old_count - 1
+				not_done = 0
+			else:
+				not_done = 0
+				count = old_count - 1
+				print('returning to old count now which is: ' + str(count))
+
+
+
 	count=count+1
 
 
